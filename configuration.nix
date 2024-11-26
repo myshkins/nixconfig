05fddceb085994181185d8bf31fb71d52189d248
myshkins <alex@ak0.io>
2024-11-26 11:09:17 -0800

    add ack, remove autoconf

--- configuration.nix
+++ configuration.nix
@@ -112,8 +112,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-     autoconf
-     automake
+     ack
      gammastep
      gcc
      gnumake
